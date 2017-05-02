11964a18b74b60fa2ede7522b381183383eaef21
Rodrigo Torres <juanro.toes@gmail.com>
2017-05-02 12:22:24 -0500

    jtorres: Finished rest service to fetch all the clients in json format.

--- team-practical-test-web/src/main/java/com/team/presentation/rest/RestClientService.java
+++ team-practical-test-web/src/main/java/com/team/presentation/rest/RestClientService.java
@@ -5,6 +5,10 @@ package com.team.presentation.rest;
 
 import java.util.List;
 
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import javax.persistence.EntityManager;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
@@ -23,7 +27,25 @@ public class RestClientService {
 	
 	/** The Client Control **/
 	//TODO: change this to fix the depedency injection problem with jersey.
-	private ClientTransactionalControl clientService = new ClientTransactionalControl();
+	private ClientTransactionalControl clientService;
+	
+	
+
+	/**
+	 * Default constructor
+	 * TODO: change this to fix the depedency injection problem with jersey.
+	 * @throws NamingException 
+	 */
+	public RestClientService() throws NamingException {
+		// obtain the initial JNDI context
+        Context initCtx = new InitialContext();
+        // perform JNDI lookup to obtain container-managed entity manager
+        EntityManager entityManager = (EntityManager) 
+            initCtx.lookup("java:/TeamManager");
+		clientService = new ClientTransactionalControl(entityManager);
+	}
+
+
 
 	@GET
 	@Path("/clients")
